3e2f07c0c6d758aa78ea6b46119ab651d12c6c8b
MarcoVCunha <126254095+MarcoVCunha@users.noreply.github.com>
2025-07-21 23:49:45 -0300

    Motions

---
+++ src/components/Contact.jsx
@@ -0,0 +1,47 @@
+import { CONTACT } from "../constants";
+import { motion } from "framer-motion";
+
+const Contact = () => {
+  return (
+    <div className="border-t border-stone-900 pb-20">
+      <motion.h2
+        whileInView={{ opacity: 1 }}
+        initial={{ opacity: 0 }}
+        transition={{ duration: 1 }}
+        className="my-10 text-center text-4xl"
+      >
+        Entre em contato
+      </motion.h2>
+      <div className="text-center tracking-tighter">
+        <motion.p
+          whileInView={{ opacity: 1 }}
+          initial={{ opacity: 0 }}
+          transition={{ duration: 1 }}
+          className="my-4"
+        >
+          {CONTACT.address}
+        </motion.p>
+        <motion.p
+          whileInView={{ opacity: 1 }}
+          initial={{ opacity: 0 }}
+          transition={{ duration: 1 }}
+          className="my-4"
+        >
+          {CONTACT.phoneNo}
+        </motion.p>
+        <motion.a
+          whileInView={{ opacity: 1 }}
+          initial={{ opacity: 0 }}
+          transition={{ duration: 1 }}
+          href={`https://mail.google.com/mail/?view=cm&to=${CONTACT.email}&su=Assunto&body=Mensagem`}
+          className="border-b "
+          target="_blank"
+        >
+          {CONTACT.email}
+        </motion.a>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -1,31 +1,100 @@
-import { DiGit, DiJavascript1, DiMysql, DiNodejsSmall } from "react-icons/di";
+import {
+  DiGit,
+  DiJava,
+  DiJavascript1,
+  DiMysql,
+  DiNodejsSmall,
+} from "react-icons/di";
 import { RiReactjsLine } from "react-icons/ri";
 import { SiMongodb } from "react-icons/si";
+import { animate, motion } from "framer-motion";
+
+const iconsVariants = (duration) => ({
+  initial: { y: 10 },
+  animate: {
+    y: [10, -10],
+    transition: {
+      duration: duration,
+      ease: "linear",
+      repeat: Infinity,
+      repeatType: "reverse",
+    },
+  },
+});
 
 const Technologies = () => {
   return (
     <div className="pb-24 ">
-      <h2 className="my-20 text-center text-4xl">Tecnologias</h2>
-      <div className="flex flex-wrap items-center justify-center gap-4">
-        <div>
-            <RiReactjsLine className="text-8xl text-cyan-400"/>
-        </div>
-        <div className="p-4">
-            <DiJavascript1 className="text-8xl text-yellow-300"/>
-        </div>
-        <div className="p-4">
-            < DiNodejsSmall className="text-8xl text-green-500"/>
-        </div>
-        <div className="p-4">
-            <SiMongodb className="text-8xl text-green-700"/>
-        </div>
-        <div className="p-4">
-            <DiMysql className="text-8xl text-blue-900"/>
-        </div>
-        <div className="p-4">
-            <DiGit className="text-8xl text-orange-700"/>
-        </div>
-      </div>
+      <motion.h2
+        whileInView={{ opacity: 1, y: 0 }}
+        initial={{ opacity: 0, y: -100 }}
+        transition={{ duration: 1.5 }}
+        className="my-20 text-center text-4xl"
+      >
+        Tecnologias
+      </motion.h2>
+      <motion.div
+        whileInView={{ opacity: 1, y: 0 }}
+        initial={{ opacity: 0, x: -100 }}
+        transition={{ duration: 1.5 }}
+        className="flex flex-wrap items-center justify-center gap-4"
+      >
+        <motion.div
+          initial="initial"
+          animate="animate"
+          variants={iconsVariants(2.5)}
+        >
+          <RiReactjsLine className="text-8xl text-cyan-400" />
+        </motion.div>
+        <motion.div
+          initial="initial"
+          animate="animate"
+          variants={iconsVariants(3.5)}
+          className="p-4"
+        >
+          <DiJavascript1 className="text-8xl text-yellow-300" />
+        </motion.div>
+        <motion.div
+          initial="initial"
+          animate="animate"
+          variants={iconsVariants(5)}
+          className="p-4"
+        >
+          <DiNodejsSmall className="text-8xl text-green-500" />
+        </motion.div>
+        <motion.div
+          initial="initial"
+          animate="animate"
+          variants={iconsVariants(2)}
+          className="p-4"
+        >
+          <DiJava className="text-8xl text-red-900" />
+        </motion.div>
+        <motion.div
+          initial="initial"
+          animate="animate"
+          variants={iconsVariants(6)}
+          className="p-4"
+        >
+          <SiMongodb className="text-8xl text-green-700" />
+        </motion.div>
+        <motion.div
+          initial="initial"
+          animate="animate"
+          variants={iconsVariants(4)}
+          className="p-4"
+        >
+          <DiMysql className="text-8xl text-blue-900" />
+        </motion.div>
+        <motion.div
+          initial="initial"
+          animate="animate"
+          variants={iconsVariants(3)}
+          className="p-4"
+        >
+          <DiGit className="text-8xl text-orange-700" />
+        </motion.div>
+      </motion.div>
     </div>
   );
 };
